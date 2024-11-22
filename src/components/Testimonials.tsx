f294f6b263731324655a5444f539e5deed540976
Samuel Santos Marques <samuellsmarquess@gmail.com>
2024-11-22 18:53:07 -0300

    sdfgh

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -11,8 +11,8 @@ export const Testimonials = () => {
                         <h2>Cada cliente importa!</h2>
                     </span>
                     <p>
-                        Quem já pediu sabe da qualidade das nossas receitas, estamos tirando aquela ideia de que
-                        comida congelada tem de ser algo sem gosto, acompanhe abaixo os testemunhos de quem já comprou e aprovou.
+                    Quem já testou a Certto Fibra sabe que nossa internet é sinônimo de velocidade e estabilidade. Estamos mudando a ideia de que internet boa tem que ser cara ou complicada.
+                     Confira abaixo os depoimentos de nossos clientes e descubra como a Certto Fibra está transformando a experiência de conexão em sua casa ou empresa.
                     </p>
                 </header>
 
